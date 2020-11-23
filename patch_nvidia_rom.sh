a28c016e60ea9ca07452afde466a7473f415847f
Cat-Gaming <47046268+Cat-Gaming@users.noreply.github.com>
2020-11-23 15:08:26 -0500

    Create patch_nvidia_rom.sh

---
+++ patch_nvidia_rom.sh
@@ -0,0 +1,6 @@
+sudo apt install python3
+
+# change this to your gpu
+wget https://www.techpowerup.com/vgabios/194101/PNY.GTX1050.2048.170116.rom -O NVIDIA.rom # download rom for nvidia gtx 1050
+
+python3 nvidia_vbios_vfio_patcher.py -i NVIDIA.rom -o NVIDIA_PATCHED.rom
