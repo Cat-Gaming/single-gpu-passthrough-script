d545562969c326f8383fcc3bf287b81ea22430a1
Cat-Gaming <47046268+Cat-Gaming@users.noreply.github.com>
2020-11-23 15:16:18 -0500

    Update main_script.sh

--- main_script.sh
+++ main_script.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 echo "Script Started."
+
+sudo ./check_cpu_and_install_deps.sh
+# patch nvidia rom
+# then edit vm to fix nvidia 43 error
+sudo ./setup_vfio_hooks.sh
+
+sudo cp start_vm.sh /etc/libvirt/hooks/qemu.d/prepare/begin/start.sh
+sudo cp revert_vm.sh /etc/libvirt/hooks/qemu.d/release/end/revert.sh
+
+echo "Please Reboot."
