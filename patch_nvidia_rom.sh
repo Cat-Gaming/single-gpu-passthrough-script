f3c28a02fc71cef83ef971ea4c5dda8656dd780b
Cat-Gaming <47046268+Cat-Gaming@users.noreply.github.com>
2020-11-23 15:22:52 -0500

    Update patch_nvidia_rom.sh

--- patch_nvidia_rom.sh
+++ patch_nvidia_rom.sh
@@ -3,4 +3,8 @@ sudo apt install python3
 # change this to your gpu
 wget https://www.techpowerup.com/vgabios/194101/PNY.GTX1050.2048.170116.rom -O NVIDIA.rom # download rom for nvidia gtx 1050
 
-python3 nvidia_vbios_vfio_patcher.py -i NVIDIA.rom -o NVIDIA_PATCHED.rom
+git clone https://github.com/Matoking/NVIDIA-vBIOS-VFIO-Patcher.git
+
+cd NVIDIA-vBIOS-VFIO-Patcher
+python3 nvidia_vbios_vfio_patcher.py -i ../NVIDIA.rom -o NVIDIA_PATCHED.rom
+cd ..
